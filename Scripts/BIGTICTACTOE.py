c9ab5f3ebe892eaf738032b8cba64a6aa05ae66b
TyTheCoder854 <tybearlytton@gmail.com>
2025-02-07 09:46:23 -0800

    The other script
    Tic Tac Toe but you cant play it and it BIG

---
+++ Scripts/BIGTICTACTOE.py
@@ -0,0 +1,31 @@
+def idk():
+    print("     |     |     ")
+    print("     |     |     ")
+    print("-----|-----|-----")
+    print("     |     |     ")
+    print("     |     |     ")
+    print("-----|-----|-----")
+    print("     |     |     ")
+    print("     |     |     ")
+
+def idk2():
+    idk()
+    idk()
+    idk()
+    idk()
+    idk()
+    idk()
+
+def idk3():
+    idk2()
+    idk2()
+    idk2()
+
+def idk4():
+    idk3()
+    idk3()
+
+def idk5():
+    idk4()
+
+idk5()
